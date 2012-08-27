46f28ba8edfacd27bfdc2761e4e30051c620769c
Svilen Vassilev <svilen@rubystudio.net>
2012-08-27 20:02:44 +0300

    0.3

--- lib/gcmapper.rb
+++ lib/gcmapper.rb
@@ -15,7 +15,6 @@ module Gcmapper
     url = "http://www.gcmap.com/map?P=#{route}%0d%0a&MS=#{tr}&MR=120&MX=#{width}x#{height}&PM=#{style}"
   end
 
-private
   # disc + user code             b:disc7%2b%25U
   # disc + user_code + city      b:disc7%2b\"%25U%25+%28N\"
   # disc + airport_name          b:disc7+%A
@@ -24,7 +23,7 @@ private
     style = 'b:disc7%2b' #basic
     args[:city] == false ? style += '%25U' : style += '"%25U%25+%28N"'
     style = 'b:disc7+%A' if args[:airport_name] == true
-    return style
+    style
   end
 end
 
